29da390ffe4ab079ecb8820fb4834cc667979f29
Joe Flood <joe@tinygoose.co.uk>
2020-03-24 22:16:29 +0000

    Actually, OneShot isn't working

--- Prefabs/DeathParticles.cs
+++ Prefabs/DeathParticles.cs
@@ -5,7 +5,7 @@ public class DeathParticles : Particles2D
 {
     public async override void _Ready()
     {
-        await ToSignal(GetTree().CreateTimer(1.0f), "timeout");
+        await ToSignal(GetTree().CreateTimer(0.9f), "timeout");
         CallDeferred("queue_free");
     }
 }
