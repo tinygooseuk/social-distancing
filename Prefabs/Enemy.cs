57e2467cfcf052d067b24ae10ecdbace08f45bf1
Joe Flood <joe@tinygoose.co.uk>
2020-03-24 23:30:28 +0000

    Add pixel explosions

--- Prefabs/Enemy.cs
+++ Prefabs/Enemy.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db33a5b67fb95a970e5fe8743360e8a3a5deb0a59aca819775f6117c69b30689
-size 1867
+oid sha256:9dc1a608d79ce0028c9f0124a3a1790ee1b2b5e232c81cd821bc217c0fabd10e
+size 3292
---
+++ Prefabs/Pixel.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7d4516b7cf74a36cc2e43166ae2da21819eb6c44b79d8d9ddc9465644a7aa311
+size 384
