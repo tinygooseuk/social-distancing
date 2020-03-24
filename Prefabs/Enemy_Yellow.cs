f69e05ccf7d0bcdcf2ba3617ce04ea4724cfd977
Joe Flood <joe@tinygoose.co.uk>
2020-03-24 23:30:54 +0000

    Make yellow enemies awful!

--- Prefabs/Enemy_Yellow.cs
+++ Prefabs/Enemy_Yellow.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a52841f5084636800afcd46f43568c1707fcf3afee21363e36fd1ada7afe10a
-size 758
+oid sha256:97a177306d11703dc8231df711c73808e69cf8fe80f61b4cd5dc42512fd88178
+size 765
