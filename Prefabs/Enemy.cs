fab0239a5b26822c2f15aa346190e7e2f3d40b65
Joe Flood <joe@tinygoose.co.uk>
2020-03-26 22:15:07 +0000

    Better pixelisation performance

--- Prefabs/Enemy.cs
+++ Prefabs/Enemy.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9dc1a608d79ce0028c9f0124a3a1790ee1b2b5e232c81cd821bc217c0fabd10e
-size 3292
+oid sha256:0b072e48fe5215f5de4750fc50c649436aa7a2855a83d76ef22f9e7c5d3b2287
+size 3853
