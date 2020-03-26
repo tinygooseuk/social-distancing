73aea1c357ac052c330f34ab96ee17de1dbc92df
Joe Flood <joe@tinygoose.co.uk>
2020-03-26 22:33:27 +0000

    Add lots of Util classes, rename folder

---
+++ Util/AssetRef.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:db32b520518c262e34402249fedc47196d071dfc3b1e0a8c3f7ccb603a552064
+size 1770
---
+++ Util/AsyncUtil.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:be496179e5cb5141f207d0b30ae2466e922d46bc3b48ed4048f1fd544eb82a23
+size 678
