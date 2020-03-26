d48a98727e6907f8d78e9f356dc973083816ffe2
Joe Flood <joe@tinygoose.co.uk>
2020-03-26 22:33:41 +0000

    Add Scene/Asset ref system from CP

--- Prefabs/Character.cs
+++ Prefabs/Character.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9181c6c46ee60476ee58d1b5be8889d7500ad0235a532bbb5cd9ca814d5b284
-size 4728
+oid sha256:2a257b7537675047034c99426d5ae104425a8a6dd61151fc17ff6a61b5e96325
+size 4720
---
+++ R.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d94d690492e9c82ebda4fe68640512d5d3ea18986ae14b4187d1114e0963a86e
+size 315
