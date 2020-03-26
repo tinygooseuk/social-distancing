d83e66f1075a3becca0166c07fd87c97bd75f0d1
Joe Flood <joe@tinygoose.co.uk>
2020-03-26 22:33:27 +0000

    Add lots of Util classes, rename folder

---
+++ Util/AssetRef.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Threading.Tasks;
+using Godot;
+
+public class Asset<T> where T : Resource
+{
+    public string ResourcePath { get; protected set; }
+
+    protected T Object;
+    public bool IsLoaded => Object != null;
+
+    protected Asset(string path)
+    {
+        ResourcePath = path;
+
+        if (OS.IsDebugBuild() && !ResourceLoader.Exists(path))
+        {
+            GD.Print($"*** ERROR: Broken AssetRef: {path} ***");
+        }
+    }
+    public static implicit operator Asset<T>(string path) => new Asset<T>(path);
+
+    public T Load()
+    {
+        if (Object == null)
+        {
+            Object = GD.Load<T>(ResourcePath);;
+        }
+        
+        return Object;
+    }
+
+    public async Task<T> LoadAsync()
+    {
+        if (Object == null)
+        {
+            Object = await AsyncUtil.LoadAsync<T>(ResourcePath);
+        }        
+        
+        return Object;
+    }
+
+    public void Unload()
+    {
+        Object = null;
+    }
+
+    public void Reload()
+    {
+        Unload();
+        Load();
+    }
+}
+
+public class Scene<T> : Asset<PackedScene> where T : Node
+{
+    private Scene(string scenePath) : base(scenePath) 
+    {
+
+    }
+    public static implicit operator Scene<T>(string path) => new Scene<T>(path);
+    
+    public T Instance()
+    {
+        if (Object == null) 
+        {
+            Load();
+        }
+         
+        // Loaded and held - just instance
+        return (T)Object.Instance();        
+    } 
+     
+    public async Task<T> InstanceAsync()
+    {
+        if (Object == null) 
+        {
+            await LoadAsync();
+        }
+         
+        // Loaded and held - just instance
+        return (T)Object.Instance();        
+    } 
+}
