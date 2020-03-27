33f6aa7dcc5f3fcf0d7622fc8274dbfcf7d1c179
Joe Flood <joe@tinygoose.co.uk>
2020-03-27 15:03:16 +0000

    Re-work AssetRef system

--- Util/AssetRef.cs
+++ Util/AssetRef.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db32b520518c262e34402249fedc47196d071dfc3b1e0a8c3f7ccb603a552064
-size 1770
+oid sha256:64be822a1d81c950b46057d7193c9ab870f4b3cf5d920e26556906e242a7f322
+size 2104
