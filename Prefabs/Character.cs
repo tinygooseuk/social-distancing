80837c45753ac02e043d6f262168b26631f4d59d
Joe Flood <joe@tinygoose.co.uk>
2020-03-29 00:19:52 +0000

    Big update:
    - Add passthru jumping
    - Add fallthru
    - Fix double jump bug (finally!)
    - Reposition bullet muzzle
    - Fix pixel collisions
    - Delete all rooms to start again (!!)
    - Move score to right
    - Make TileSet reusable resource

--- Prefabs/Character.cs
+++ Prefabs/Character.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02cdbdb4bb7a585cdb8095d7864d7ae93877d6dce9eb68e2fce0c9e168dca664
-size 6975
+oid sha256:0a7b7f542df14aba8bc46e91a3786e323792cbbb04a9078408aef86b847509e4
+size 8707
