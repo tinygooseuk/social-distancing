d45329b0e96779a189876f63aa03a9b9c70dae88
Joe Flood <joe@tinygoose.co.uk>
2020-03-30 00:20:57 +0100

    Add drop sound & each player has a different pitch

--- Prefabs/Character.cs
+++ Prefabs/Character.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0613fcbdbe27bb7cc7843df48e39643ae53d8b01432d2a35a3dbc952e59c42f5
-size 8929
+oid sha256:f4e9b9edb80b4a86c1db3716ae1be45b6297a72d3b5297df1bbce8dda474a491
+size 9235
