ba11f743e199ec8f58d5b65c50c1257af2e50eab
Joe Flood <joe@tinygoose.co.uk>
2020-04-01 00:35:15 +0100

    Fix >2 player "error" on restart

--- Game.cs
+++ Game.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e891637948b300094dff254653de27eb47ad558058d2183ae60da3254f58986f
-size 7423
+oid sha256:30e1ab61195a2ab8ab41e7fdb83c753d80fa7a7ad50f64544bdb06936080512f
+size 7100
