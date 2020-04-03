1e195047ffa3450eb7528ad70de27c2417927e27
Joe Flood <joe@tinygoose.co.uk>
2020-04-03 17:52:49 +0100

    Make [Down] sufficient for dropping with keyboard

--- Prefabs/Character.cs
+++ Prefabs/Character.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36ae8181bfbf1af80b04d23cdc03ac81e1dfe90fb70c33e6b9933129dd00ba98
-size 12137
+oid sha256:5d9c84a8c66b38dc0a64141b996847d38a391df215a9be1120318e19a58e7da2
+size 12755
