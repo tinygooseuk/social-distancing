307dba86451149ebdb7ddd39a0e1bd15cab15047
Joe Flood <joe@tinygoose.co.uk>
2020-04-04 23:38:36 +0100

    Re-jig behaviours

---
+++ Scripts/Behaviour/BehaviourModifiers.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0f604c3c50d502bd53f70deff24c59be65fc1810eb804ad14662a68d90231321
+size 2123
---
+++ Scripts/Behaviour/ShootBehaviours.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:df95dd9d2de35b9900f09312239b8e56d28d726dfa17abcbdcb4b67aded71a6b
+size 894
