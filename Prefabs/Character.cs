c33849ee79a894632f7a49c8819c7b19270a4cc8
Joe Flood <joe@tinygoose.co.uk>
2020-04-07 22:44:46 +0100

    Don't allow jump, drop, or shoot when round complete

--- Prefabs/Character.cs
+++ Prefabs/Character.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96dad04d18d6135ffb53f48ad611c0353ec0b645449c6ba5909a96e65612805d
-size 13213
+oid sha256:ad43aa36a8e9b2d414be5fab5c43a11173e8dd8dfdf8878c6ef68169c0ecfc2e
+size 13437
