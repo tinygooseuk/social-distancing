149013f9c5dcb367316da82bed28bf20972ddf3e
Joe Flood <joe@tinygoose.co.uk>
2020-04-08 23:44:56 +0100

    Add nice screen transition

---
+++ Scripts/TitleCard.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e7232245c79e45b8469f17b21d0bbefddbba64ba17d6d663c514978bf1b0333d
+size 1232
---
+++ Shaders/ScreenTransition.shader
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:80ffedded1ef5e65f5b3d741616b319521f24482d823ab8aaed6d50cc959e3d3
+size 494
