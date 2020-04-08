d1036a10d7b818ce498ec8cdf0f7e0cf75ba5237
Joe Flood <joe@tinygoose.co.uk>
2020-04-08 18:57:15 +0100

    Don't show debug UI by default

--- Scripts/DebugUI.cs
+++ Scripts/DebugUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d462aea31ff48d4dc69e6a9d7803c0756422674bc9fc811d50db6516502177ad
-size 1988
+oid sha256:4eca3cda565bfc5e2d92e032b4332c2411ea68a6ca54e11d1c307859371e8af5
+size 1976
