9354c39d95857159c7fc349b2ec01a4b40c28be4
Joe Flood <joe@tinygoose.co.uk>
2020-04-10 17:34:05 +0100

    Don't bob if not jumping

--- Prefabs/Character.cs
+++ Prefabs/Character.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad43aa36a8e9b2d414be5fab5c43a11173e8dd8dfdf8878c6ef68169c0ecfc2e
-size 13437
+oid sha256:b20c90a1d01cbe8ccd592cc323b9f811e3ad6367a51643d2f65a971ce2828169
+size 13486
