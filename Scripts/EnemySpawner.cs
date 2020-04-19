f10eab8269d38a504604d02e36018cfae33d1fb4
Joe Flood <joe@tinygoose.co.uk>
2020-04-19 23:07:01 +0100

    Spawn enemies only when close

--- Scripts/EnemySpawner.cs
+++ Scripts/EnemySpawner.cs
@@ -4,14 +4,22 @@ using System;
 
 public class EnemySpawner : Position2D
 {
-    [Export] private Array<PackedScene> EnemyScenes = new Array<PackedScene>();
+    [Export] private readonly Array<PackedScene> EnemyScenes = new Array<PackedScene>();
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        CallDeferred("Spawn");
+        //CallDeferred("Spawn");
     }
 
+    private void OnBodyEntered(Node other)
+    {
+        if (other is Character)
+        {
+            Spawn();
+        }
+    }
+    
     private void Spawn()
     {
         PackedScene scene = EnemyScenes[(int)(GD.Randi() % EnemyScenes.Count)];
